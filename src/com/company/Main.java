9ed88b22408a1600de2140589dcc0e24b4420fd3
silleloekke <sillekea@gmail.com>
2022-02-25 14:12:26 +0100

    shouldnt have done that

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,23 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello1");
-        System.out.println("hello2");
+        Main obj = new Main();
+        Scanner input = new Scanner (System.in);
+        //obj.askAboutAge(input);
+        obj.askAboutName(input);
+    }
 
+    }
+    public static void askAboutName (Scanner input){
+        System.out.println("Hvad er dit navn?");
+        String navn = input.next();
 
+        System.out.println("Hej " + navn);
 
 
     }
+
+
 }
