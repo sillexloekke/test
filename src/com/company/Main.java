9a35fd175247190dfefe0f4be90af44a0cd9efba
silleloekke <sillekea@gmail.com>
2022-02-25 14:23:56 +0100

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Main obj = new Main();
         Scanner input = new Scanner (System.in);
-        //obj.askAboutAge(input);
-        obj.askAboutName(input);
+        obj.askAboutAge(input);
+        askAboutName(input);
     }
-
+    public void askAboutAge(Scanner input){
+        System.out.println("Hvor gammel er du?");
+        int alder = input.nextInt();
+        System.out.println("Okay så du er " + alder);
     }
     public static void askAboutName (Scanner input){
         System.out.println("Hvad er dit navn?");
